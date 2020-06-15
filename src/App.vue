dc84b43997aa6c92f3050751d34aa6256a40ff04
Rafli Ramadhan <rafliramdhn@gmail.com>
2020-06-15 22:21:14 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,125 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section id="app" class="bg-white">
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <router-link class="navbar-brand" to="/">
+          <img
+            src="@/assets/logo.png"
+            active-class="active"
+            width="60"
+            height="60"
+            alt
+          />
+        </router-link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <router-link class="nav-link" active-class="active" to="/"
+                >WORK</router-link
+              >
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" active-class="active" to="/about"
+                >ABOUT ME</router-link
+              >
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" active-class="active" to="/roadmap"
+                >BLOG</router-link
+              >
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" active-class="active" to="/roadmap"
+                >CONTACT</router-link
+              >
+            </li>
+            <li class="nav-item">
+              <span class="nav-divider"></span>
+            </li>
+            <li class="nav-item">
+              <router-link
+                class="btn btn-outline-dark rounded-pill px-3"
+                active-class="active"
+                to="/login"
+                >HIRE ME</router-link
+              >
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    <router-view />
+  </section>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,700;1,400&family=Montserrat:wght@300;400;500;600;700&family=Red+Hat+Display:wght@400;500;700&display=swap");
+
+.btn-dark {
+  color: #fff !important;
+}
+.active > .nav-link,
+.navbar-nav .nav-link.active {
+  color: #31de31 !important;
+}
+.navbar-light .navbar-nav .nav-link {
+  color: #000;
+}
+.navbar-light .navbar-nav .nav-link:hover {
+  color: #31de31 !important;
+}
+/* .text-purple {
+  color: #9d9d9e !important;
+} */
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  overflow-x: hidden;
+  font-family: "Montserrat", sans-serif;
+  font-weight: 400;
+  line-height: 30px;
+}
+.text-redhat {
+  font-family: "Red Hat Display", sans-serif;
+}
+.navbar {
+  padding-top: 30px;
+  padding-right: 1rem;
+  padding-bottom: 30px;
+  padding-left: 1rem;
+}
+.navbar-nav .nav-divider {
+  width: 1px;
+  height: calc(100% - 8px);
+  display: inline-block;
+  margin: 4px 1.25rem;
+}
+.navbar-dark .navbar-nav .nav-divider {
+  background-color: #282646;
+}
+.navbar-expand-lg .navbar-nav .nav-link {
+  padding-right: 1.25rem !important;
+  padding-left: 1.25rem !important;
+}
+.font-weight-light {
+  font-weight: 300;
+}
+.btn-outline-purple {
+  color: #fff;
+  border-color: #fff;
+}
+.btn-outline-purple:hover {
+  color: #fff;
+  background-color: #491d8c;
+  border-color: #491d8c;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <main>
+    <div class="container border-bottom py-5 mb-5">
+      <div class="text-redhat">
+        <h2 class="text-muted  font-weight-light">Hello, I'm</h2>
+        <h1 class="font-weight-bolder">Rafli Ramadhan</h1>
+        <h5 class="text-muted ">
+          Fullstack Web Developer
+        </h5>
+        <div class="media-sosial mt-4">
+          <a href="http://" class="pr-3">
+            <img src="@/assets/logo.png" width="40" />
+          </a>
+          <a href="http://" class="pr-3">
+            <img src="@/assets/logo.png" width="40" />
+          </a>
+          <a href="http://" class="pr-3">
+            <img src="@/assets/logo.png" width="40" />
+          </a>
+          <a href="http://" class="pr-3">
+            <img src="@/assets/logo.png" width="40" />
+          </a>
+          <a href="http://" class="pr-3">
+            <img src="@/assets/logo.png" width="40" />
+          </a>
+        </div>
+        <div class="action-link pt-5 mb-5">
+          <a
+            href="http://"
+            class="btn btn-dark rounded-pill py-2 px-4 mr-4  font-weight-bolder"
+          >
+            HIRE ME
+          </a>
+          <a
+            href="http://"
+            class="btn btn-outline-dark rounded-pill py-2 px-4 mr-4  font-weight-bolder"
+          >
+            SEE MY RESUME
+          </a>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
