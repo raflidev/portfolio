30855782744dd8ed47ab1e68cabad28f093e758f
Rafli Ramadhan <rafliramdhn@gmail.com>
2020-06-16 04:09:16 +0700

    add work layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,9 @@
   <main>
     <div class="container border-bottom py-5 mb-5">
       <div class="text-redhat">
-        <h2 class="text-muted  font-weight-light">Hello, I'm</h2>
+        <h2 class="text-muted font-weight-light">Hello, I'm</h2>
         <h1 class="font-weight-bolder">Rafli Ramadhan</h1>
-        <h5 class="text-muted ">
-          Fullstack Web Developer
-        </h5>
+        <h5 class="text-muted">Fullstack Web Developer</h5>
         <div class="media-sosial mt-4">
           <a href="http://" class="pr-3">
             <img src="@/assets/logo.png" width="40" />
@@ -27,16 +25,51 @@
         <div class="action-link pt-5 mb-5">
           <a
             href="http://"
-            class="btn btn-dark rounded-pill py-2 px-4 mr-4  font-weight-bolder"
-          >
-            HIRE ME
-          </a>
+            class="btn btn-dark rounded-pill py-2 px-4 mr-4 font-weight-bolder"
+          >HIRE ME</a>
           <a
             href="http://"
-            class="btn btn-outline-dark rounded-pill py-2 px-4 mr-4  font-weight-bolder"
-          >
-            SEE MY RESUME
-          </a>
+            class="btn btn-outline-dark rounded-pill py-2 px-4 mr-4 font-weight-bolder"
+          >SEE MY RESUME</a>
+        </div>
+      </div>
+    </div>
+    <div class>
+      <div class="container">
+        <h2 class="font-weight-bolder text-center">Working</h2>
+        <div class="py-4 text-center">
+          <button class="btn btn-outline-secondary mr-3 rounded-pill">Desktop</button>
+          <button class="btn btn-outline-secondary mr-3 rounded-pill">Web</button>
+          <button class="btn btn-outline-secondary mr-3 rounded-pill">UI</button>
+        </div>
+        <div class="row">
+          <div class="col-lg-4">
+            <img src="@/assets/post/post.jpg" class="rounded my-4" width="325" srcset />
+          </div>
+          <div class="col-lg-4">
+            <img src="@/assets/post/post.jpg" class="rounded my-4" width="325" srcset />
+          </div>
+          <div class="col-lg-4">
+            <img src="@/assets/post/post.jpg" class="rounded my-4" width="325" srcset />
+          </div>
+          <div class="col-lg-4">
+            <img src="@/assets/post/post.jpg" class="rounded my-4" width="325" srcset />
+          </div>
+          <div class="col-lg-4">
+            <img src="@/assets/post/post.jpg" class="rounded my-4" width="325" srcset />
+          </div>
+          <div class="col-lg-4">
+            <img src="@/assets/post/post.jpg" class="rounded my-4" width="325" srcset />
+          </div>
+          <div class="col-lg-4">
+            <img src="@/assets/post/post.jpg" class="rounded my-4" width="325" srcset />
+          </div>
+          <div class="col-lg-4">
+            <img src="@/assets/post/post.jpg" class="rounded my-4" width="325" srcset />
+          </div>
+          <div class="col-lg-4">
+            <img src="@/assets/post/post.jpg" class="rounded my-4" width="325" srcset />
+          </div>
         </div>
       </div>
     </div>
