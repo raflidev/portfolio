8b4b9bc9fe777bf3062c8d1fa1adbfa67643d3f0
Rafli Ramadhan <rafliramdhn@gmail.com>
2020-08-01 17:10:08 +0700

    add vuex

--- src/main.js
+++ src/main.js
@@ -5,8 +5,10 @@ import router from "./router";
 Vue.config.productionTip = false;
 import "bootstrap/dist/css/bootstrap.min.css";
 import "material-icons/iconfont/material-icons.css";
+import store from './store'
 
 new Vue({
   router,
-  render: (h) => h(App),
+  store,
+  render: (h) => h(App)
 }).$mount("#app");
